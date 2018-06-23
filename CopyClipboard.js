16b4880fcd5e00df0db86ea284c3493433201e2d
Max Base <MaxBaseCode@Gmail.Com>
2018-06-23 19:29:05 +0430

    Update CopyClipboard.js

--- CopyClipboard.js
+++ CopyClipboard.js
@@ -11,6 +11,28 @@
 ;(function(window,document)
 {
 	"use strict";
+	var text_default="Text!"
+	/**
+	* @variable systemPasteReady
+	*
+	* @goal : true | false
+	*
+	**/
+    var systemPasteReady = false;
+	/**
+	* @variable systemPasteContent
+	*
+	* @goal : pasted string for cache
+	*
+	**/
+    var systemPasteContent;
+	/**
+	* @variable textarea
+	*
+	* @goal : temp textarea
+	*
+	**/
+	var textarea;
 	/**
 	* @function send
 	*
@@ -32,14 +54,34 @@
 		xhttp.send();
 	};
 	/**
-	* @function copy
+	* @function copy_selected
+	*
+	* @goal : Copy selected text to clipboard.
+	*
+	* @return void
+	**/
+	var copy_selected=function()
+	{
+		//soon
+	};
+	/**
+	* @function copy_text
 	*
 	* @goal : Copy text to clipboard.
 	*
 	* @return void
 	**/
-	var copy=function(text)
+	var copy_text=function(text)
 	{
+		/*
+		var textarea = document.createElement("textarea");
+		textarea.setAttribute("style","width:0px;border:0;display:none;opacity:0;");
+		document.body.appendChild(textarea);
+		textarea.value = text;
+		textarea.select();
+		document.execCommand('copy');
+		document.body.removeChild(textarea);
+		*/
 		function selectElementText(element)
 		{
 			if (document.selection)
@@ -64,26 +106,85 @@
 		element.remove();
 	};
 	/**
-	* @function paste
+	* @function copy_target
 	*
-	* @goal : Paste text from clipboard.
+	* @goal : Copy value of a element to clipboard.
 	*
 	* @return void
 	**/
-	var paste=function()
+	var copy_target=function(target)
 	{
-
+		var text=text_default;
+		if(target.val !== undefined)
+		{
+			text=target.val;
+		}
+		else if(target.innerHTML !== undefined)
+		{
+			text=target.innerHTML;
+		}
+		else if(target.value !== undefined)
+		{
+			text=target.value;
+		}
+		copy_text(text);
 	};
 	/**
 	* @function get
 	*
 	* @goal : Get text from clipboard.
 	*
+	* @return void
+	**/
+	var get=function(target)
+	{
+		if(window.clipboardData)
+		{
+			return window.clipboardData.getData("Text");
+		}
+		function waitForPaste()
+		{
+			if(systemPasteReady==false)
+			{
+				setTimeout(waitForPaste,250);
+			}
+			else
+			{
+				var text=systemPasteContent;
+				systemPasteReady = false;
+				document.body.removeChild(textarea);
+				textarea = null;
+				return text;
+			}
+		}
+		textarea = document.createElement("textarea");
+		textarea.setAttribute("style","width:0px;border:0;display:none;opacity:0;");
+		document.body.appendChild(textarea);
+		textarea.select();
+		return waitForPaste();
+	};
+	/**
+	* @function paste
+	*
+	* @goal : Paste text from clipboard.
+	*
 	* @return string
 	**/
-	var get=function()
+	var paste=function(target)
 	{
-
+		var text=get();
+		if(target.val !== undefined)
+		{
+			target.val=text;
+		}
+		else if(target.innerHTML !== undefined)
+		{
+			target.innerHTML=text;
+		}
+		else if(target.value !== undefined)
+		{
+			target.value=text;
+		}
 	};
 	/**
 	* @function text
@@ -94,7 +195,12 @@
 	**/
 	var text=function(from)
 	{
-
+		var text=text_default;
+		if(from.hasAttribute("data-copy-text"))
+		{
+			text=from.getAttribute("data-copy-text");
+		}
+		copy_text(text);
 	};
 	/**
 	* @function from
@@ -105,8 +211,35 @@
 	**/
 	var from=function(from)
 	{
-
+		var text=text_default;
+		var target=undefined;
+		if(from.hasAttribute("data-copy-from"))
+		{
+			target=document.querySelector(from.getAttribute("data-copy-from"));
+		}
+		if(target === undefined)
+		{
+			copy_text(text);
+		}
+		else
+		{
+			copy_target(target);
+		}
 	};
+	/**
+	* @struct systemPasteListener
+	*
+	* @goal : Listener key press whool of web page.
+	*
+	* @return function
+	**/
+	function systemPasteListener(evt)
+	{
+		systemPasteContent = evt.clipboardData.getData("text/plain");
+		systemPasteReady = true;
+		evt.preventDefault();
+	}
+	window.addEventListener('paste',systemPasteListener);
 	/**
 	* @struct goscroll
 	*
@@ -117,7 +250,9 @@
 	window.copy=
 	{
 		//manual using
-		copy:copy,
+		copy_selected:copy_selected,
+		copy_text:copy_text,
+		//use
 		paste:paste,
 		text:text,
 		from:from,
@@ -141,6 +276,18 @@
 				item.onclick=function()
 				{
 					window.copy.text(this);
+				};
+			}
+		});
+		//////////////////////////////////////////////////////////////////
+		data_copys = document.querySelectorAll("[data-copy-from]");
+		data_copys.forEach(function(item)
+		{
+			if(item.onclick === null)//onclick not exists
+			{
+				item.onclick=function()
+				{
+					window.copy.from(this);
 				};
 			}
 		});
